70e6925111921f12bed129709bdb7da6dcbe1bcb
hotardaw <hotardaw@gmail.com>
2025-01-05 18:59:38 -0600

    Long overdue push for docker-compose, servers.json, start-app.sh

--- start-app.sh
+++ start-app.sh
@@ -4,6 +4,6 @@ docker-compose up -d --build
 sleep 5
 
 # Open tabs in chrome
-open http://localhost:8080    # frontend
-open http://localhost:8081    # backend
+# open http://localhost:8080    # frontend
+# open http://localhost:8081    # backend
 open http://localhost:8083    # pgAdmin
